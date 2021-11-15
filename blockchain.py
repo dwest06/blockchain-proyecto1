a0f7ff14fb7216badfa3812bd6d07ef6168498c8
David Rodriguez <dave00dark@gmail.com>
2021-11-15 11:06:09 -0400

    Cosas del Greg

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,4 @@
-from hashlib import sha256
+import hashlib
 from time import time
 
 class Transaction:
@@ -17,6 +17,17 @@ class Transaction:
 
 class Block():
 
+    def __init__(self, index, transactions_list, timestamp, previous_block_hash, nonce=0):
+        # No se si dejarlo ya que es el index de donde se ecuentre en la blockchain
+        self.index = index
+        self.previous_block_hash = previous_block_hash
+        # Data
+        self.block_data = "-".join(transactions_list) + "-" + previous_block_hash
+        self.transactions_list = transactions_list
+        self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+        self.timestamp = timestamp
+        self.nonce = nonce
+
     def minar(self, difficulty):
         # Metodo para minar bloque
         
@@ -26,8 +37,14 @@ class Block():
 class BlockChain():
 
     def __init__(self, difficulty):
-        pass
+        self.difficulty = difficulty
+        self.chain = []
 
+    def create_genesis_block(self):
+        # A function to generate genesis block and appends it to the chain. The block has index 0, previous_block_hash as 0, and a valid hash.
+        genesis_block = Block(0, [], 0, "0")
+        # genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
 
     def minar(self, bloque: Block):
         # Minar el bloque
